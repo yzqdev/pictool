04348806341a91f2797be218cdf8af3dc4168419
yzqdev <yzqdev@outlook.com>
2022-07-13 19:14:09 +0800

    update mihoyo api

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,8 +1,8 @@
 import http from "@/utils/http";
 
 export function getCosApi(lastId: string) {
-  return http.get("/photo/cos/" + lastId);
+  return http.get(`/mihoyo/post/wapi/getForumPostList?forum_id=49&gids=2&is_good=false&is_hot=false&last_id=${lastId}&page_size=20&sort_type=1`);
 }
 export function getTongrenApi(lastId: string) {
-  return http.get("/photo/tongren/" + lastId);
+  return http.get(`/mihoyo/post/wapi/getForumPostList?forum_id=29&gids=2&is_good=false&is_hot=false&last_id=${lastId}&page_size=20&sort_type=1`);
 }
